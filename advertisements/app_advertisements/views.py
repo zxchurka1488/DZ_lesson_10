458b67ebdc65f2bfb746dd5f9bcc6ac50d7d6f1d
zxchurka1488 <139868359+zxchurka1488@users.noreply.github.com>
2023-08-23 12:55:05 +0300

    Дз урок 10 модуль 4

---
+++ advertisements/app_advertisements/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Advertisement
+from .forms import AdvertisementForm
+from django.urls import reverse
+
+def index(request):
+    advertisements = Advertisement.objects.all()
+    context = {"advertisements": advertisements}
+    return render(request, 'index.html', context)
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
+def advertisement_post(request):
+    if request.method == "POST":
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            advertisement = Advertisement(**form.cleaned_data)
+            advertisement.user = request.user
+            advertisement.save()
+            url = reverse('index')
+            return redirect(url)
+    else:
+        form = AdvertisementForm()
+    context = {'form': form}
+    return render(request, 'advertisement-post.html', context)
